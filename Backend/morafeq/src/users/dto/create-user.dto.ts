66ffd79d2b04ba892f89b1f0f1053083f4596e75
Mohamed_Atia <muhamedatia22@gmail.com>
2026-06-03 10:33:37 +0300

    make change in user module

--- Backend/morafeq/src/users/dto/create-user.dto.ts
+++ Backend/morafeq/src/users/dto/create-user.dto.ts
@@ -1,1 +1,5 @@
-export class CreateUserDto {}
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-wrapper-object-types */
+export class CreateUserDto {
+     avatarUrl!: String;
+}
