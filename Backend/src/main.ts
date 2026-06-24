acd643ae7db72a23286f496a4a49fd96b6080014
Mohamed_Atia <muhamedatia22@gmail.com>
2026-06-24 14:53:22 +0300

    dddddddd

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -35,12 +35,12 @@ async function bootstrap() {
   credentials: true,
 });
 
-app.enableCors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  credentials: true,
-  methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-});
+// app.enableCors({
+//   origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+//   credentials: true,
+//   methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+// });
 
   // All routes start with /api/v1
 //   app.setGlobalPrefix('api/v1');
