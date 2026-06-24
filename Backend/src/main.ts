a4a2b7d20b31f9bc1862595ff6ee09204b165812
Mohamed_Atia <muhamedatia22@gmail.com>
2026-06-24 14:39:15 +0300

    modify cors

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -29,13 +29,13 @@ async function bootstrap() {
     }),
   );
 
-  // Allow frontend to talk to backend
-//   app.enableCors(
-//     {
-//     origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-//     credentials: true,
-//   }
-// );
+   Allow frontend to talk to backend
+   app.enableCors(
+     {
+     origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+     credentials: true,
+   }
+ );
 
 app.enableCors({
   origin: process.env.FRONTEND_URL || 'http://localhost:5173',
